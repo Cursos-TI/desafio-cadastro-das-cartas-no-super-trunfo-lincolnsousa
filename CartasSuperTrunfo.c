f8801e8386422406e2cc0f33f2553ffc89efad95
lincolnsousa <lincolnreis1810@gmail.com>
2025-08-06 15:24:31 +0000

    Atividade 1 concluida

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,7 +7,51 @@
 //Teste larissa
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
+   //Carta1
+   char estado[2] = "B";
+   char codigoDoEstado[5] = "BAHI";
+   char nomeDaCidade[20] = "Salvador";
+   int populacao = 3000000;
+   float area = 170.500;
+   float prod = 4705.5;
+   int pontosTuristicos = 80;
+
+   //Carta2
+   char estado2[2] = "S";
+   char codigoDoEstado2[5] = "CATA";
+   char nomeDaCidade2[20] = "Palhoça";
+   int populacao2 = 100000;
+   float area2 = 70.500;
+   float prod2 = 705.5;
+   int pontosTuristicos2 = 15;
+
+
+    //Visualizar no terminal
+    //Carta1
+    printf("Carta1\n");
+    printf("Estado: %s\n", estado);
+    printf("Código: %s\n", codigoDoEstado);
+    printf("Nome da cidade: %s\n", nomeDaCidade);
+    printf("População: %d de Habitantes\n", populacao);
+    printf("Área: %.2f km²\n", area);
+    printf("PIB: %.2f\n", prod);
+    printf("Número de pontos turísticos: %d\n", pontosTuristicos);
+    
+    printf("\n");
+    
+    //Carta2
+    printf("Carta2\n");
+    printf("Estado: %s\n", estado2);
+    printf("Código: %s\n", codigoDoEstado2);
+    printf("Nome da cidade: %s\n", nomeDaCidade2);
+    printf("População: %d de Habitantes\n", populacao2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: %.2f\n", prod2);
+    printf("Número de pontos turísticos: %d\n", pontosTuristicos2);
+
+    return 0;
+}
+// Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
     // Cadastro das Cartas:
@@ -17,6 +61,3 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-
-    return 0;
-}
